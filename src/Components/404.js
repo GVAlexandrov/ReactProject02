3dae423cc9efd0f55c9697113eddb4ce5ed902e1
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-04-17 17:01:01 +0300

    Some initial changes

---
+++ src/Components/404.js
@@ -0,0 +1,13 @@
+const Error404 = () => {
+    return (
+        <main>
+            <section className="notifications">
+                <p className="notification-message">Page not found</p>
+                <img src="sadgace.png" alt="sad-fase" className="sad-face" />
+                <h1 className="not-found">404 - Page Not Found</h1>
+            </section>
+        </main>
+    )
+}
+
+export default Error404;
