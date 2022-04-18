3681f6e180d16b7a4c4cafda3e84e183828328c0
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-04-18 22:08:23 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Route, Switch } from 'react-router-dom';
+import { Routes, Route, Switch } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
+import Login from './Components/Login';
 import Error404 from './Components/404';
 
 import style from './App.module.css';
@@ -11,7 +12,10 @@ function App() {
     <div className={style.app}>
       <Header />
 
-      <Home />
+      <Routes>
+        <Route path='/login' element={<Login />} />;
+        <Route path='/' element={<Home />} />;
+      </Routes>
 
       <Footer />
     </div >
