c1de5394bd22c282484e6ffc96857d71dd3ea3da
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-04-18 20:53:06 +0300

    ADD Home
    New Home view started

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from './Components/Home';
 import Error404 from './Components/404';
 
 import style from './App.module.css';
@@ -9,6 +10,9 @@ function App() {
   return (
     <div className={style.app}>
       <Header />
+
+      <Home />
+
       <Footer />
     </div >
   );
---
+++ src/Components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <main>
+            <p class="guest-message">MoneyGone? The best expense tracker tool of all time! Аre you new here? <a href="#">Register</a> yourself!</p>
+        </main>
+    )
+}
+
+export default Home;
