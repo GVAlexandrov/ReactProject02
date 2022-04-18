a689a86fb3bfedcbe134c38af2bdf6c217c0ccf1
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-04-18 22:08:15 +0300

    UPDATE Home
    Home page updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
         <main>
-            <p class="guest-message">MoneyGone? The best expense tracker tool of all time! Аre you new here? <a href="#">Register</a> yourself!</p>
+            <p className="guest-message">MoneyGone? The best expense tracker tool of all time! Аre you new here? <a href="#">Register</a> yourself!</p>
         </main>
     )
 }
