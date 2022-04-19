33aee718b5ea11352a0da6a5612ad568f085f571
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-04-19 21:01:38 +0300

    ADD Register

---
+++ src/Components/Register.js
@@ -0,0 +1,26 @@
+const Register = () => {
+    <main>
+        <form>
+            <h1>Register</h1>
+
+            <p>Personal info</p>
+            <label for="usernmae">Username</label>
+            <input id="username" type="text" placeholder="JustMyself123..." />
+
+            <label for="usernmae">Password</label>
+            <input id="password" type="password" placeholder="******" />
+
+            <label for="repeat-password">Repeat Password</label>
+            <input id="repeat-password" type="password" placeholder="******" />
+            <hr>
+                <p>Account</p>
+                <label for="amount">Amount</label>
+                <input id="amount" type="text" placeholder="$125.90" />
+
+                <button type="submit">Register</button>
+            </hr>
+        </form>
+    </main>
+}
+
+export default Register;
