dbfaddbcccb482b934e21311653862296599acaa
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-01 19:30:50 +0300

    UPDATE App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Register from './Components/Register';
 import Login from './Components/Login';
 import NewExpense from './Components/NewExpense';
 import Expenses from './Components/Expenses';
+import ExpenseDetails from './Components/ExpenseDetails';
+import EditExpense from './Components/EditExpense';
 import Profile from './Components/Profile';
 import Error404 from './Components/404';
 
@@ -17,12 +19,12 @@ function App() {
   const [userEmail, setUserEmail] = useState(null);
 
   useEffect(() => {
-    auth.onAuthStateChange(setUserEmail);
+    auth.onAuthStateChanged(setUserEmail);
   }, []);
 
   return (
     <div className={style.app}>
-      <Header />
+      <Header userEmail={userEmail} setUserEmail={setUserEmail} />
 
       <Routes>
         <Route path='/' element={<Home />} />;
@@ -30,6 +32,9 @@ function App() {
         <Route path='/login' element={<Login />} />;
         <Route path='/new-expense' element={<NewExpense />} />;
         <Route path='/expenses' element={<Expenses />} />;
+        <Route path='/expenses/:id/details' element={<ExpenseDetails />} />;
+        <Route path='/expenses/:id/details/edit' element={<EditExpense />} />;
+
         <Route path='/profile' element={<Profile />} />;
       </Routes>
 
