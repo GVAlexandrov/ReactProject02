dd8e7bf17413e965f1535ea83dace8b385c9734e
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-01 19:31:08 +0300

    ADD firebaseConfig

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA0mH0wytd71Nr7dghMhfYPE0VMbMvzicQ",
+    authDomain: "react-project-01-55fc9.firebaseapp.com",
+    databaseURL: "https://react-project-01-55fc9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "react-project-01-55fc9",
+    storageBucket: "react-project-01-55fc9.appspot.com",
+    messagingSenderId: "445436551332",
+    appId: "1:445436551332:web:659202652fcdc688fa8631"
+};
