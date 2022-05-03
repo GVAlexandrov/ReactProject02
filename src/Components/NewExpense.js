8c9835344cc294145b4a1bf37e23f5f45005587d
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-03 20:21:55 +0300

    UPDATE NewExpense

--- src/Components/NewExpense.js
+++ src/Components/NewExpense.js
@@ -1,72 +1,137 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+
+import * as expenseService from '../services/services';
+import validate from '../validations/expensesValidations';
+
+
 const NewExpense = () => {
+    let [merchantError, setMerchantError] = useState("");
+    let [priceError, setPriceError] = useState("");
+
+    const navigate = useNavigate();
+
     let options = [
-        { option: 'Advertising', value: 'advertising' },
-        { option: 'Benefits', value: 'benefits' },
+        { option: 'All', value: 'all' },
         { option: 'Car', value: 'car' },
-        { option: 'Equipment', value: 'equipment' },
-        { option: 'Fees', value: 'fees' },
+        { option: 'Clothes', value: 'clothes' },
         { option: 'Insurance', value: 'insurance' },
-        { option: 'Interest', value: 'interest' },
-        { option: 'Home Office', value: 'home-office' },
-        { option: 'Labor', value: 'labor' },
+        { option: 'Maintenance', value: 'maintenance' },
+        { option: 'Food', value: 'food' },
+        { option: 'Rent', value: 'rent' },
+        { option: 'Taxes', value: 'taxes' },
+        { option: 'Travel', value: 'travel' },
+        { option: 'Utilities', value: 'utilities' },
+        { option: 'Other', value: 'other' },
     ]
+
+    let onNewExpenseSubmitHandler = (e) => {
+        e.preventDefault();
+
+        let { merchant, price, curency, category, description } = e.target;
+        const merchantError = validate.merchant(merchant.value);
+        const priceError = validate.price(price.value);
+
+        if (merchantError) {
+            setMerchantError(merchantError);
+            setTimeout(() => {
+                setMerchantError('');
+            }, 1500);
+        }
+
+        if (priceError) {
+            setPriceError(priceError);
+            setTimeout(() => {
+                setPriceError('');
+            }, 1500);
+        }
+
+        if (merchantError || priceError) return;
+
+        expenseService
+            .addNewExpense(
+                merchant.value,
+                price.value,
+                curency.value,
+                category.value,
+                description.value)
+            .then(resp => {
+                if (resp.statusText === "OK") {
+                    navigate('/expenses');
+                }
+            })
+            .catch(console.log);
+    }
+
 
     return (
         <main>
-            <form>
-                <h1>Expense</h1>
+            <form onSubmit={onNewExpenseSubmitHandler}>
+                <h1>New Expense</h1>
+
 
                 <div className="form-control">
-                    <label htmlFor="merchant">Merchant*</label>
-                    <input id="merchant" type="text" placeholder="Shoes" />
+                    <label htmlFor="merchant">Expense*</label>
+
+                    <input id="merchant" name="merchant" type="text" placeholder="Expense" />
                 </div>
 
+                {merchantError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{merchantError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
+
+
                 <div className="form-control">
-                    <label htmlFor="total">Total*</label>
-                    <input id="total" type="text" placeholder="123.00" />
-                    <select name="vault" id="vault">
-                        <option value="bgn">BGN лв</option>
+                    <label htmlFor="total">Price*</label>
+
+                    <input id="total" name="price" type="number" placeholder="52.00" />
+
+                    <select className="vault" name="curency" id="vault">
+                        <option value="bgn">BGN</option>
+                        {/* <option value="eur">EUR</option> */}
                     </select>
                 </div>
 
+                {priceError
+                    ? (
+                        <div className="notifications" >
+                            <p className="notification-message">{priceError}</p>
+                        </div>
+                    )
+                    : (<></>)
+                }
+
                 <div className="form-control">
                     <label htmlFor="category">Category*</label>
-                    <select name="category" id="category">
-                        <option disabled selected value="default">Select category...</option>
+
+                    <select className="category" name="category" id="category">
+                        {/* <option disabled selected value="default">Select category...</option> */}
                         {options.map(x => {
                             return (
                                 <option value={x.value}>{x.option}</option>
                             )
                         })}
-                        {/* 
-                        <option value="maintenance">Maintenance</option>
-                        <option value="materials">Materials</option>
-                        <option value="meals-and-entertainment">Meals and Entertainment</option>
-                        <option value="office-supplies">Office Supplies</option>
-                        <option value="other">Other</option>
-                        <option value="professional-services">Professional Services</option>
-                        <option value="rent">Rent</option>
-                        <option value="taxes">Taxes</option>
-                        <option value="travel">Travel</option>
-                        <option value="utilities">Utilities</option> */}
                     </select>
                 </div>
 
+
                 <div className="form-control">
                     <label htmlFor="description">Description*</label>
-                    <input id="description" type="text" placeholder="Shoes description..." />
+
+                    <input id="description" name="description" type="text" placeholder="Expense description..." />
                 </div>
 
-                <div className="form-control">
-                    <label htmlFor="report">Report: </label>
-                    <input type="checkbox" id="report" />
-                </div>
 
                 <button type="Submit">Save</button>
-
             </form>
         </main>
-    )
-}
+    );
+};
+
 
 export default NewExpense;
