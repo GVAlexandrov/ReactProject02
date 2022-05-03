ababa4b51fa8a98000b07f062b525e226fe9623e
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-03 20:21:21 +0300

    UPDATE authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,17 +1,21 @@
-import { auth } from '../config/firebaseInit';
+import { firebaseLogin, firebaseRegister, firebaseSignOut } from '../config/firebaseInit';
 
 export function register(email, pass) {
-    return auth.createUserWithEmailAndPassword(email, pass)
-        .then(() => auth.signInWithEmailAndPassword(email, pass))
+    return firebaseRegister(email, pass)
+        .then(() => login(email, pass))
         .catch((error) => console.log(error))
 }
 
 
 export function login(email, pass) {
-    return auth.signInWithEmailAndPassword(email, pass);
+    return firebaseLogin(email, pass);
 }
 
 export function activeUser(uid, email) {
     localStorage.setItem('uid', uid);
     localStorage.setItem('email', email);
+}
+
+export function logout() {
+    return firebaseSignOut();
 }
