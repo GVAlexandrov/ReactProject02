2b2b006daead63ef6822e50ce1d187e660c278fb
GVAlexandrov <80593140+GVAlexandrov@users.noreply.github.com>
2022-05-03 20:21:43 +0300

    UPDATE Profile

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,31 +1,52 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+// import { NavLink } from 'react-router-dom';
+
+import { getAllExpenses, getTotalAmount } from '../services/services';
 
 const Profile = () => {
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [totalExpenses, setTotalExpenses] = useState(0);
+    const [availableAmount, setAvailableAmount] = useState(0);
+
+    useEffect(() => {
+        getAllExpenses()
+            .then(amount => setTotalExpenses(amount))
+            .catch(console.log);
+
+        getTotalAmount()
+            .then(amount => setTotalAmount(amount))
+            .catch(console.log);
+    }, []);
+
+    useEffect(() => {
+        setAvailableAmount(totalAmount - totalExpenses);
+    }, [totalAmount, totalExpenses])
+
     return (
         <main>
-            <table class="expenses-info">
+            <table className="expenses-info">
                 <thead>
                     <tr>
-                        <th colspan="2">Expenses</th>
+                        <th colSpan="2">Expenses</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>Total Amount:</td>
-                        <td>3887.85 BGN</td>
+                        <td>{totalAmount.toFixed(2)} BGN</td>
                     </tr>
                     <tr>
                         <td>Total Merches</td>
-                        <td>1416</td>
+                        <td>{totalExpenses.toFixed(2)} BGN</td>
                     </tr>
                 </tbody>
             </table>
 
-            <div class="current-amount">
-                <p>Available amount: <span>1234.00 BGN</span></p>
+            <div className="current-amount">
+                <p>Available amount: <span>{availableAmount.toFixed(2)} BGN</span></p>
             </div>
         </main>
-    )
-}
+    );
+};
 
 export default Profile;
